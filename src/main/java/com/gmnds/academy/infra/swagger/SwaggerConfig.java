0595a9094bec8b42e1249ffeb291d437db2ae30a
GMNDS <gabriel@gmnds.com>
2025-11-12 01:17:17 -0300

    Reconfigura `application.properties` e OpenAPi

--- src/main/java/com/gmnds/academy/infra/swagger/SwaggerConfig.java
+++ src/main/java/com/gmnds/academy/infra/swagger/SwaggerConfig.java
@@ -3,8 +3,12 @@ package com.gmnds.academy.infra.swagger;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,5 +21,15 @@ public class SwaggerConfig {
                 .license(new License().name("MIT").url("https://opensource.org/licenses/MIT")
                 )
         );
+    }
+
+    @Bean
+    public OpenApiCustomizer openApiCustomizer() {
+        return openApi -> {
+            openApi.servers(List.of(
+                    new Server().url("https://api.astrum.app.br").description("Produção"),
+                    new Server().url("http://localhost:8080").description("Local server")
+            ));
+        };
     }
 }
